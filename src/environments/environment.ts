0fc11bc04591cd9d3992cb612374d513d9df34ad
HengRanut-h <ranuttv4@gmail.com>
2026-08-17 01:00:33 +0700

    deploy frontend and fix api backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,10 @@
-export const environment = { production: false, apiBaseUrl: '/api/v1', hubBaseUrl: '/hubs' };
+export const environment = { production: false, 
+  // apiBaseUrl: '/api/v1',
+  //  hubBaseUrl: '/hubs' 
+
+     apiBaseUrl:
+    'https://hotel-management-system-j4ml.onrender.com/api/v1',
+
+  hubBaseUrl:
+    'https://hotel-management-system-j4ml.onrender.com/hubs'
+  };
